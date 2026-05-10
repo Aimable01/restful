dcbf42b3f03f9be04454aea3374f39a7962329bf
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-10 20:34:44 +0200

    feat: main app config

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
