61d45b67aab821e59c4402326c7d78077351de31
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-11 14:03:40 +0200

    feat: car entry model

---
+++ backend/src/modules/entries/entry.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const entrySchema = new mongoose.Schema(
+  {
+    plateNumber: String,
+    parkingCode: String,
+    entryDateTime: {
+      type: Date,
+      default: Date.now(),
+    },
+    exitDateTime: {
+      type: Date,
+      default: null,
+    },
+    status: {
+      type: String,
+      default: "IN",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Entry", entrySchema);
