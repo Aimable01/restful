3a365afc310c69c7866613d60783998d26da3373
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-13 11:39:48 +0200

    feat: add swagger doc on routes

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -3,6 +3,27 @@ import { register, login } from "./auth.controller";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *  post:
+ *      summary: Register a new user.
+ *      tags: [Auth]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                          password:
+ *                              type: string
+ *      responses:
+ *          201:
+ *              description: User registered successfully
+ */
 router.post("/register", register);
 router.post("/login", login);
 
