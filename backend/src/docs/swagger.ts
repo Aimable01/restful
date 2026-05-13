06a806f62c69446a32d0dc6a3dba7cced121f48f
Aimable01 <aimable.kwizera14@gmail.com>
2026-05-13 11:33:41 +0200

    fix: swagger ui visual

--- backend/src/docs/swagger.ts
+++ backend/src/docs/swagger.ts
@@ -2,6 +2,7 @@ import swaggerJsDoc from "swagger-jsdoc";
 
 export const specs = swaggerJsDoc({
   definition: {
+    openapi: "3.0.0",
     info: {
       title: "Parking system",
       version: "1.0.0",
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,13 +4,18 @@ dotenv.config();
 
 import app from "./app";
 
+import swaggerUi from "swagger-ui-express";
+
 import authRoutes from "./modules/auth/auth.routes";
 import parkingRoutes from "./modules/parking/parking.routes";
 import entryRoutes from "./modules/entries/entry.routes";
 
 import { connectDB } from "./config/db";
+import { specs } from "./docs/swagger";
 
 connectDB();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/parking", parkingRoutes);
